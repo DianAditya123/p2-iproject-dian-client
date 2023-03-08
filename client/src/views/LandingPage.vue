e7b631d51696b586ae801d9bac23ed762f42acdc
unknown <dianaditya200@gmail.com>
2023-03-08 13:32:59 +0700

    revisi

--- client/src/views/LandingPage.vue
+++ client/src/views/LandingPage.vue
@@ -5,19 +5,20 @@ export default {
 </script>
 
 <template>
-    <img class="hero" src="https://ik.imagekit.io/wnkpreh30/YT_Logo5.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1675759186848"
-            alt="">
+    <img class="hero"
+        src="https://ik.imagekit.io/wnkpreh30/YT_Logo5.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1675759186848"
+        alt="">
     <section class="about">
         <div class="main">
             <img src="https://ik.imagekit.io/wnkpreh30/photo1673926909__13_.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1675753268747"
                 alt="">
-                <div class="about-text">
-                    <h2>About Us</h2>
-                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                        printer
-                        took a galley of type and scrambled it to make a type specimen book.</p>
-                        <button type="button" class="c">Contact Us</button>
-                </div>
+            <div class="about-text">x
+                <h2>About Us</h2>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                    printer
+                    took a galley of type and scrambled it to make a type specimen book.</p>
+                <button type="button" class="c">Contact Us</button>
+            </div>
         </div>
     </section>
 
@@ -26,31 +27,37 @@ export default {
             <h2>Our Services</h2>
         </div>
 
-        <div class="box">
-            <div class="card">
+        <div className="box">
+            <div className="card">
                 <center>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" id="a" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" id="a" fill="currentColor"
+                        className="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
                 </center>
-                  <h5>Fotografi</h5>
-                  <div class="pra">
-                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <p style="text-align: center;"><a href="#" class="button">Read More</a></p>
-                  </div>
+                <h5>Fotografi</h5>
+                <div className="pra">
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                        printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p><a href="#" className="button">Read More</a></p>
+                </div>
             </div>
 
-            <div class="card">
+            <div className="card">
                 <center>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" id="a" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" id="a" fill="currentColor"
+                        className="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
                 </center>
-                  <h5>Fotografi</h5>
-                  <div class="pra">
-                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <p style="text-align: center;"><a href="#" class="button">Read More</a></p>
-                  </div>
+                <h5>Fotografi</h5>
+                <div className="pra">
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                        printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p><a href="#" className="button">Read More</a></p>
+                </div>
             </div>
         </div>
     </section>
